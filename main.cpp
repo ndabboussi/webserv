eed25a18459720e7e24332d290cce7bc3a43d92a
Nina Dbbs <dabboussi.nina@gmail.com>
2025-09-17 18:08:21 +0200

    parsing.cpp 1st draft

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
-#include "Location.hpp"
+#include "Server.hpp"
 
 int	main(int ac, char **av)
 {
-	std::string	configFile = "conf/nanana.conf";
+	std::string	configFile = "./conf/nanana.conf";
 	if (ac > 2)
 		return (std::cout << "Error: too many arguments." << std::endl, 1);
-	else
+	else if (ac == 2)
 		configFile = av[1];
+	std::vector<Server>	servers;
 	try
 	{
+		parsing(servers, configFile);
 		//call parsing function
 	}
 	catch(const std::exception &e)
---
+++ srcs/parsing/parser.cpp
@@ -0,0 +1,9 @@
+# include "Server.hpp"
+
+void	parsing(std::vector<Server> &servers, std::string configFile)
+{
+	// 1. file opening, if unable, throw and fuck off
+	// 2. function : getline while oef, when keyword server or location is found, open dedicated function
+	// 3. in parsingLocation and parsingServer 
+	// 4. if eof and not in inital function, it means missing bracket = EXCEPTION
+}
