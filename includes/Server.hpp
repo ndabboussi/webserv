33fbc5b895a4f347666b8f232c5fa570b5e3bd6f
Maxime Prokosch <maxime.prokosch@free.fr>
2025-09-18 12:11:01 +0200

    begin of parsing, setter and getters in Locations.cpp

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -2,15 +2,13 @@
 # define SERVER_HPP
 
 # include "Location.hpp"
-# include <map>
-# include <vector>
 
 class	 Server : public Location
 {
 	private:
-		std::map<std::string, std::string>	_data;
-		// int			_port;
-		// std::string	_name;
+		int			_port;
+		std::string	_name;
+
 	public:
 
 };
