8f6105ffb10aec0dee7f718baa0bb6741f7148af
Nina Dbbs <dabboussi.nina@gmail.com>
2025-09-19 19:45:50 +0200

    first try receiving a request + sending response

--- srcs/connect.cpp
+++ srcs/connect.cpp
@@ -5,7 +5,35 @@
 #include <sys/socket.h> // socket, bind, listen, accept
 #include "Server.hpp"
 
-int launchServer()
+struct HttpRequest
+{
+	std::string method;
+	std::string path;
+	std::string version;
+};
+
+static HttpRequest parseHttpRequest(const std::string &rawRequest)
+{
+	HttpRequest req;
+
+	std::istringstream requestStream(rawRequest);
+	requestStream >> req.method >> req.path >> req.version;
+
+	if (req.method.empty() || req.path.empty() || req.version.empty())
+	{
+		std::cerr << RED "Malformed HTTP request received" << RESET << std::endl;
+	}
+	else
+	{
+		std::cout << YELLOW "[>] Parsed Request: "
+					<< req.method << " " << req.path << " " << req.version
+					<< RESET << std::endl;
+	}
+
+	return req;
+}
+
+int launchServer(const std::vector<Server> &servers)
 {
 	// 1. Create a socket
 	int server_fd;
@@ -13,28 +41,39 @@ int launchServer()
 	if (server_fd == -1)
 	{
 		//perror("socket");
-		std::cerr << "Failed to create socket. errno: " << errno << std::endl;
+		std::cerr << RED "Failed to create socket. errno: " << errno << RESET << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
 	const int	flag = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(int)) < 0)
 	{
-		std::cerr << "Error. errno: " << errno << std::endl;
+		std::cerr << RED "Error. errno: " << errno << RESET << std::endl;
+		close(server_fd);
 		return 1;
+	}
+
+	//	1.1. Determine which port to use
+	int	port = 8080;
+	if (!servers.empty())
+	{
+		if (servers[0].getPort() > 0)
+		{
+			port = servers[0].getPort();
+		}
 	}
 
 	// 2. Listen to port 8080 (0.0.0.0:8080)
 	struct sockaddr_in sockAddress;
 	sockAddress.sin_family = AF_INET;
-	sockAddress.sin_addr.s_addr = INADDR_ANY; // toutes les interfaces
-	sockAddress.sin_port = htons(8080);
+	sockAddress.sin_addr.s_addr = INADDR_ANY;
+	sockAddress.sin_port = htons(port);
 	
 	// 3. Link socket to the address
 	if (bind(server_fd, (struct sockaddr *)&sockAddress, sizeof(sockAddress)) < 0)
 	{
 		//perror("bind");
-		std::cerr << "Failed to bind to port 8080. errno: " << errno << std::endl;
+		std::cerr << RED "Failed to bind to port 8080. errno: " << errno << RESET << std::endl;
 		close(server_fd);
 		exit(EXIT_FAILURE);
 	}
@@ -43,42 +82,81 @@ int launchServer()
 	if (listen(server_fd, 10) < 0)
 	{
 		//perror("listen");
-		std::cerr << "Failed to listen on socket. errno: " << errno << std::endl;
+		std::cerr << RED "Failed to listen on socket. errno: " << errno << RESET << std::endl;
 		close(server_fd);
 		exit(EXIT_FAILURE);
 	}
 
-	std::cout << "Server running on http://localhost:8080" << std::endl;
+	std::cout << GREEN "Server running on http://localhost:" << port << RESET <<  std::endl;
 
 	// 5. Grab a connection from the queue
-	socklen_t sockAddrLen = sizeof(sockAddress);
-	std::memset(&sockAddress, 0, sizeof(sockAddress));
-	int	client_fd;
-	client_fd = accept(server_fd, (struct sockaddr *)&sockAddress, &sockAddrLen);
-	if (client_fd < 0)
+	while (true)
 	{
-		//perror("accept");
-		std::cerr << "Failed to grab connection. errno: " << errno << std::endl;
-		close(server_fd);
-		exit(EXIT_FAILURE);
-	}
+		struct sockaddr_in clientAdrr;
+		socklen_t	clientLen = sizeof(clientAdrr);
+		int client_fd = accept(server_fd, (struct sockaddr *)&clientAdrr, &clientLen);
+		if (client_fd < 0)
+		{
+			//perror("accept");
+			std::cerr << RED "Failed to grab connection. errno: " << errno << RESET << std::endl;
+			close(server_fd);
+			continue;
+		}
 
-	// 6. Read from the connection
-	char buffer[1024];
-	int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-	if (bytes_read > 0)
-	{
+		// 6. Read from the connection
+		char buffer[4096];
+		int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+		if (bytes_read <= 0)
+		{
+			close(client_fd);
+			continue;
+		}
+		// if (bytes_read > 0)
+		// {
+		// 	buffer[bytes_read] = '\0';
+		// 	std::cout << GREEN "[>] HTTP Request received:\n" << buffer << RESET << std::endl;
+		// }
+
 		buffer[bytes_read] = '\0';
-		std::cout << "The message was: \n" << buffer << std::endl;
+		HttpRequest request = parseHttpRequest(buffer);
+
+		if (request.path.find("/pictures") == 0)
+		{
+			std::string filePath = "conf/data/test/pictures/herbe.jpg";
+			std::ifstream file(filePath.c_str(), std::ios::binary);
+			if (file)
+			{
+				std::vector<char> fileContent((std::istreambuf_iterator<char>(file)),
+												std::istreambuf_iterator<char>());
+				std::ostringstream response;
+				response << "HTTP/1.1 200 OK\r\n";
+				response << "Content-Type: image/jpeg\r\n";
+				response << "Content-Length: " << fileContent.size() << "\r\n";
+				response << "Connection: close\r\n\r\n";
+
+				std::string headers = response.str();
+				send(client_fd, headers.c_str(), headers.size(), 0);
+				send(client_fd, fileContent.data(), fileContent.size(), 0);
+
+				std::cout << GREEN "[<] Sent image: herbe.jpg (" << fileContent.size() << " bytes)" << RESET << std::endl;
+			}
+			else
+			{
+				std::string notFound = "HTTP/1.1 404 Not Found\r\nContent-Length: 13\r\n\r\n404 Not Found";
+				send(client_fd, notFound.c_str(), notFound.size(), 0);
+			}
+		}
+		else
+		{
+			// 7. Send a message to the connection
+			std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 19\r\n\r\nHello from webserv!";
+			std::cout << GREEN << "[<] Response sent to client." << RESET << std::endl;
+			send(client_fd, response.c_str(), strlen(response.c_str()), 0);
+		}
+
+		close(client_fd);
+
 	}
-
-	// 7. Send a message to the connection
-	 std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 25\r\n\r\nHello from webserv!";
-
-	send(client_fd, response.c_str(), strlen(response.c_str()), 0);
-
-	// Close the connections
-	close(client_fd);
 	close(server_fd);
 
 	return 0;
