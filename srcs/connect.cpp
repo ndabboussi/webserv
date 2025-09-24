54dee5b589d4560d6292af6a389f1f45e2c969d9
Nina Dbbs <dabboussi.nina@gmail.com>
2025-09-24 19:31:32 +0200

    working on select

--- srcs/connect.cpp
+++ srcs/connect.cpp
@@ -111,59 +111,114 @@ int launchServer(std::vector<Server> &servers)
 		servers[i].setSocketFd(server_fd);
 	}
 
-	acceptConnect(servers[0]);
-	// while (true)
-	// {
-	// 	struct sockaddr_in clientAdrr;
-	// 	socklen_t clientLen = sizeof(clientAdrr);
+	// acceptConnect(servers[0]);
+	while (true)
+	{
+		struct sockaddr_in clientAdrr;
+		socklen_t clientLen = sizeof(clientAdrr);
 
-	// 	int client_fd = accept(servers[0].getSocketFd(), (struct sockaddr *)&clientAdrr, &clientLen);
-	// 	if (client_fd < 0)
-	// 	{
-	// 		std::cerr << RED "Failed to grab connection. errno: " << errno << RESET << std::endl;
-	// 		continue;
-	// 	}
-	// 	//char client_address[INET_ADDRSTRLEN];
-	// 	char client_address[BUFSIZE];
-	// 	inet_ntop(AF_INET, &clientAdrr, client_address, BUFSIZE);//debug
-	// 	printf(PINK "Client connection: %s\n" RESET, client_address);//debug
+		int client_fd = accept(servers[0].getSocketFd(), (struct sockaddr *)&clientAdrr, &clientLen);
+		if (client_fd < 0)
+		{
+			std::cerr << RED "Failed to grab connection. errno: " << errno << RESET << std::endl;
+			continue;
+		}
+		//char client_address[INET_ADDRSTRLEN];
+		char client_address[BUFSIZE];
+		inet_ntop(AF_INET, &clientAdrr, client_address, BUFSIZE);//debug
+		printf(PINK "Client connection: %s\n" RESET, client_address);//debug
 
-	// 	handleClient(client_fd, servers[0]);
-	// 	close(client_fd);
-	// }
+		handleClient(client_fd, servers[0]);
+		close(client_fd);
+	}
 	for(size_t i = 0; i < servers.size(); i++)
 		close(servers[i].getSocketFd());
 	return 0;
 }
 
 
-// struct ServerSocket {
-//     int fd;
-//     int port;
-//     Server *server;
-// };
-// std::vector<ServerSocket> sockets;
+fd_set readfds;
+int max_fd = -1;
+while (true) {
+    FD_ZERO(&readfds);
+    for (size_t i = 0; i < sockets.size(); ++i) {
+        FD_SET(sockets[i].fd, &readfds);
+        if (sockets[i].fd > max_fd) max_fd = sockets[i].fd;
+    }
+
+    int activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+    if (activity < 0) continue; // erreur ou signal
+
+    for (size_t i = 0; i < sockets.size(); ++i) {
+        if (FD_ISSET(sockets[i].fd, &readfds)) {
+            int client_fd = accept(sockets[i].fd, ...);
+            // Ici tu sais sur quel serveur/port le client s'est connecté :
+            handleClient(client_fd, *sockets[i].server);
+        }
+    }
+}
 
 
-// fd_set readfds;
-// int max_fd = -1;
-// while (true) {
-//     FD_ZERO(&readfds);
-//     for (size_t i = 0; i < sockets.size(); ++i) {
-//         FD_SET(sockets[i].fd, &readfds);
-//         if (sockets[i].fd > max_fd) max_fd = sockets[i].fd;
-//     }
+std::vector<int> client_fds;  // stocke tous les clients connectés
 
-//     int activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
-//     if (activity < 0) continue; // erreur ou signal
+while (true)
+{
+    fd_set readfds;
+    // FD_ZERO(&readfds);
 
-//     for (size_t i = 0; i < sockets.size(); ++i) {
-//         if (FD_ISSET(sockets[i].fd, &readfds)) {
-//             int client_fd = accept(sockets[i].fd, ...);
-//             // Ici tu sais sur quel serveur/port le client s'est connecté :
-//             handleClient(client_fd, *sockets[i].server);
-//         }
-//     }
-// }
+    int max_fd = 0;
+
+    // 1. Ajouter tous les serveurs
+    for (size_t i = 0; i < servers.size(); ++i)
+    {
+        int fd = servers[i].getSocketFd();
+        FD_SET(fd, &readfds);
+        if (fd > max_fd) max_fd = fd;
+    }
+
+    // 2. Ajouter tous les clients
+    for (size_t i = 0; i < client_fds.size(); ++i)
+    {
+        FD_SET(client_fds[i], &readfds);
+        if (client_fds[i] > max_fd) max_fd = client_fds[i];
+    }
+
+    // 3. Appeler select()
+    int ready = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+    if (ready < 0)
+    {
+        perror("select");
+        break;
+    }
+
+    // 4. Vérifier les serveurs
+    for (size_t i = 0; i < servers.size(); ++i)
+    {
+        int server_fd = servers[i].getSocketFd();
+        if (FD_ISSET(server_fd, &readfds))
+        {
+            // Nouveau client
+            int client_fd = accept(server_fd, NULL, NULL);
+            if (client_fd >= 0)
+                client_fds.push_back(client_fd);
+        }
+    }
+
+    // 5. Vérifier les clients
+    for (size_t i = 0; i < client_fds.size(); )
+    {
+        int fd = client_fds[i];
+        if (FD_ISSET(fd, &readfds))
+        {
+            if (!handleClient(fd, /*server*/)) 
+            {
+                close(fd);
+                client_fds.erase(client_fds.begin() + i);
+                continue; // ne pas incrementer i
+            }
+        }
+        ++i;
+    }
+}
 
 
