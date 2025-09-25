03d167699743524a4ad0750ebfa4c1e64b41033a
Nina Dbbs <dabboussi.nina@gmail.com>
2025-09-25 13:53:03 +0200

    launchServer() select implementation done, segfault when trying to accedde port 8082

--- includes/Location.hpp
+++ includes/Location.hpp
@@ -21,12 +21,16 @@
 # include <vector>
 
 # define GREEN "\033[32m"
-# define RESET "\033[0m"
 # define YELLOW "\033[33m"
 # define RED "\033[31m"
 # define BLUE "\033[34m"
-# define GRAY "\033[90m"
+# define GREY "\033[90m"
 # define PINK "\033[35m"
+# define RESET "\033[0m"
+
+#define BOLD "\033[1m"
+#define UNDERLINE "\033[4m"
+#define ITALIC "\033[3m"
 
 typedef enum e_methods
 {
